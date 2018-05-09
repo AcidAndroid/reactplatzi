1ec224f4c14a57366a36d37fe3d54dc551895f29
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2018-05-09 18:50:45 -0500

    ligar eventos

--- src/components/media/media.js
+++ src/components/media/media.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import './media.css'
 import PropTypes from "prop-types";
 class Media extends Component {
+    //Sintaxis es6
+// constructor(props){
+//     super(props)
+//     this.handleClick= this.handleClick.bind(this)
+// }
+
+////Usnado es6 se debe hacer asi el manejo de eventos y usar el constructor
+// handleClick(event){
+//     console.log(this.props.title)
+// }
+
+//Usando es7
+
+handleClick =(event)=>{
+    console.log(this.props.title)
+}
+
     render() {
         return (
-            <div className="row media">
+            <div className="row media" onClick={this.handleClick}>
             <div className="col s12 m7">
               <div className="card">
                 <div className="card-image">
