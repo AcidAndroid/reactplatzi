80def7975d4886b4600d193b71c3cbc0af19895a
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2018-05-09 15:54:47 -0500

    Inicio

---
+++ src/components/media/media.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Media extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                <img src="" alt="" width={260} height={160} />
+                <h3>Titulo Video</h3>
+                <p>Autor</p>
+                </div>
+            </div>
+            
+        );
+    }
+}
+
+export default Media;
