09d9bce291ba8ee39f0c5065eaf56874204d8d69
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2018-05-09 18:13:45 -0500

    Inicio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 // import './App.css';
+import 'materialize-css/dist/css/materialize.css'
 import Media from './components/media/media';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -14,7 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
-        <Media/>
+        <Media title="¿que es rd?" autor="By Pollo" image="../images/bitcoin.jpg" />
       </div>
     );
   }
--- src/components/media/media.js
+++ src/components/media/media.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import './media.css'
 
 class Media extends Component {
     render() {
         return (
-            <div>
-                <div>
-                <img src="" alt="" width={260} height={160} />
-                <h3>Titulo Video</h3>
-                <p>Autor</p>
+            <div className="row media">
+            <div className="col s12 m7">
+              <div className="card">
+                <div className="card-image">
+                  <img src={this.props.image} alt="Bitcoin" />
+                  <span className="card-title">{this.props.autor}</span>
                 </div>
+                <div className="card-content">
+                  <p>{this.props.title}</p>
+                </div>
+                <div className="card-action">
+                  <a href="#">Link a otro caso</a>
+                </div>
+              </div>
             </div>
+          </div>
             
         );
     }
