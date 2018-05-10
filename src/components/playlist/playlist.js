a5a72b2b216c12b2ca00fc272e4feced7fe0ccb3
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2018-05-10 13:08:50 -0500

    se convierte playlist en un componente funcional y media en un PureComponent

--- src/components/playlist/playlist.js
+++ src/components/playlist/playlist.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Media from '../media/media'
 import {  } from "../media/media.css";
 
-class Playlist extends Component {
-    render() {
-        const playlist = this.props.data.categories[0].playlist
+function Playlist(props){
+        const playlist =props.data.categories[0].playlist
         console.log(playlist)
         return (
             <div className="row " >
@@ -23,7 +22,7 @@ class Playlist extends Component {
             }                
             </div>
         );
-    }
+    
 }
 
 Playlist.propTypes = {
