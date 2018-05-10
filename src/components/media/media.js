213606e56ef1cf5f46f6ec89c3b6a66386fe9548
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2018-05-09 19:02:01 -0500

    manejo de estados iniciales y cambio de atributos

--- src/components/media/media.js
+++ src/components/media/media.js
@@ -8,15 +8,31 @@ class Media extends Component {
 //     this.handleClick= this.handleClick.bind(this)
 // }
 
-////Usnado es6 se debe hacer asi el manejo de eventos y usar el constructor
+////Usnado es6 se debe hacer asi el manejo de eventos y su liga ademas usar el constructor
 // handleClick(event){
 //     console.log(this.props.title)
 // }
 
-//Usando es7
+//Estes construtor ya no maneja los eventos en sintaxis es7
+//Maneja los estatus iniciales pero sigue siendo es6
+// constructor(props){
+//     super(props)
+//     this.state={
+//         autor:this.props.autor
+//     }
+// }
 
+//Asi se manjean los estatus en es7
+state={
+    autor:'By POLLO LOCO'
+}
+//Usando es7 se manejan asi en elnace de eventos
 handleClick =(event)=>{
     console.log(this.props.title)
+    //Con setState se cambia el esdao
+    this.setState({
+        autor:'By PAVOMON'
+    })
 }
 
     render() {
@@ -26,13 +42,15 @@ handleClick =(event)=>{
               <div className="card">
                 <div className="card-image">
                   <img src={this.props.image} alt="Bitcoin" />
-                  <span className="card-title">{this.props.autor}</span>
+                  {/* Asi se manejan la asignacion de atributos pero SIN considerar es estado inicial */}
+                  <span className="card-title">{this.props.autor}</span>                  
                 </div>
                 <div className="card-content">
                   <p>{this.props.title}</p>
                 </div>
+                {/* Asi se manejan la asignacion de atributos pero SI considerarando es estado inicial */}
                 <div className="card-action">
-                  <a href="#">Link a otro caso</a>
+                  <a href="#">{this.state.autor}</a>
                 </div>
               </div>
             </div>
