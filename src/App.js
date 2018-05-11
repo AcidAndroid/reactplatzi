023360c5075e212ff2eef53d5c27f61359d81f03
kelldintch <kelldintch@hotmail.com>
2018-05-10 21:44:11 -0500

    Estructura de containers y components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 // import './App.css';
 import 'materialize-css/dist/css/materialize.css'
-import Playlist from './components/playlist/playlist';
+// import Playlist from './components/playlist/playlist';
+import Home from './entries/pages/containers/home'
 import datos from './api.json'
+
 
 class App extends Component {
   render() {
     return (
       <div className="container">       
-        <Playlist data={datos}/>
+        {/* <Playlist data={datos}/> */}
+        <Home/>
       </div>
     );
   }
---
+++ src/entries/pages/containers/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import HomeLayout from '../components/home-layout'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <HomeLayout></HomeLayout>
+      </div>
+    )
+  }
+}
