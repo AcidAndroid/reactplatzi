04f802da70991df473853fd3946fa9fa9b99332f
kelldintch <kelldintch@hotmail.com>
2018-05-10 21:07:48 -0500

    Ejemplo componentes funcionales con iconos

--- src/components/playlist/playlist.js
+++ src/components/playlist/playlist.js
@@ -2,12 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Media from '../media/media'
 import {  } from "../media/media.css";
+import Play from '../icons/components/play'
+import Pause from '../icons/components/pause'
+import FullScreen from '../icons/components/fullScreen'
+import Volume from '../icons/components/volume'
 
 function Playlist(props){
         const playlist =props.data.categories[0].playlist
         console.log(playlist)
         return (
             <div className="row " >
+            <Play size={50} color="red" ></Play>
+            <Pause size={50} color="red" ></Pause>
+            <Volume size={50} color="red" ></Volume>
+            <FullScreen size={50} color="red" ></FullScreen>
             {
                 playlist.map((element)=>{
                     // Asi se manda valores uno a uno
